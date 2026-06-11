b8c147861e79e57fd28572a0a0654db1c2e2907e
ryllerfeitosa <you@ryllerfeitosa@gmail.com>
2026-06-11 08:47:36 -0300

    Script do banco finalizado, porém em análise

--- Script SQL/Script BaratieFit.sql
+++ Script SQL/Script BaratieFit.sql
@@ -1,70 +1,103 @@
-#Cria o database do projeto de baratiefit
-create database db_baratiefit_2027;
+# Cria o database do projeto de baratiefit
+create database db_baratiefit_2028;
 
-#Ativa o uso do database do produto
-use db_baratiefit_2027;
+# Ativa o uso do database do produto
+use db_baratiefit_2028;
 
+#########   TABELAS CRUDS   #########
 -- Cria a tabela de alimento
 create table tbl_alimento(
-	id int not null primary key auto_increment,
+    id int not null primary key auto_increment,
     nome varchar(100) not null,
     descricao text,
-    carboidratos_g 			decimal(8,2) not null,
-    proteinas_g 			decimal(8,2) not null,
-    lipidios_g 				decimal(8,2) not null,
-    fibras_g 				decimal(8,2) not null,
-    acucar_adicionado_g 	decimal(8,2) not null,
-    gorduras_trans_g 		decimal(8,2) not null,
-    gorduras_saturadas_g 	decimal(8,2) not null
+    carboidratos_g          decimal(8,2) not null,
+    proteinas_g             decimal(8,2) not null,
+    lipidios_g              decimal(8,2) not null,
+    fibras_g                decimal(8,2) not null,
+    acucar_adicionado_g     decimal(8,2) not null,
+    gorduras_trans_g        decimal(8,2) not null,
+    gorduras_saturadas_g    decimal(8,2) not null
 );
-
--- Adiciona a FK na tabela de alimento e cria as relacoes
-alter table tbl_alimento
-	add column id_categoria int not null,
-    add constraint FK_CATEGORIA_ALIMENTO
-		foreign key (id_categoria)
-        references tbl_categoria_alimento(id),
-	add column id_adm int not null,
-    add constraint FK_ADM_ALIMENTO
-		foreign key (id_adm)
-        references tbl_adm(id),
-	add column id_enquadramento int not null,
-    add constraint FK_ENQUADRAMENTO_ALIMENTO
-		foreign key (id_enquadramento)
-        references tbl_enquadramento(id);
 
 -- Cria a tabela de refeicao
 create table tbl_refeicao(
-	id int not null primary key auto_increment,
-	nome varchar(100) not null,
-	descricao 		text,
-	modo_preparo 	text,
-	apoio_decisao 	text
+    id int not null primary key auto_increment,
+    nome varchar(100) not null,
+    descricao       text,
+    modo_preparo    text,
+    apoio_decisao   text
 );
 
--- Adiciona a FK na tabela de refeicao e cria as relacoes
-alter table tbl_refeicao
-	add column id_tipo_refeicao int not null,
-    add constraint FK_TIPO_REFEICAO_REFEICAO
-		foreign key (id_tipo_refeicao)
-        references tbl_tipo_refeicao(id),
-	add column id_publico_alvo int not null,
-    add constraint FK_PUBLICO_ALVO_REFEICAO
-		foreign key (id_publico_alvo)
-        references tbl_publico_alvo(id),
-	add column id_adm int not null,
-    add constraint FK_ADM_ALIMENTO
-		foreign key (id_adm)
-        references tbl_adm(id);
+-- Cria a tabela de refeicao relacionada com alimento
+create table tbl_refeicao_alimento(
+    id int not null primary key auto_increment,
+    quantidade_g decimal(8,2)
+);
 
--- Cria a tabela de refeicao relacionada com restricao
+-- Cria a tabela de adm
+create table tbl_adm(
+    id int not null primary key auto_increment,
+    nome  varchar(100) not null,
+    email varchar(255) not null,
+    senha varchar(255) not null,
+    ultimo_acesso datetime
+);
+
+-- Cria a tabela de recuperacao de senha
+create table tbl_recuperacao_senha(
+    id int not null primary key auto_increment,
+    token varchar(255),
+    expira_em datetime,
+    usado tinyint(1)
+);
+
+#########   TABELAS DE DADOS ESTATICOS   #########
+-- Cria a tabela de tipo_refeicao
+create table tbl_tipo_refeicao(
+    id int not null primary key auto_increment,
+    nome varchar(50)
+);
+
+-- Cria a tabela de publico alvo
+create table tbl_publico_alvo(
+    id int not null primary key auto_increment,
+    nome varchar(100)
+);
+
+-- Cria a tabela de restricao
+create table tbl_restricao(
+    id int not null primary key auto_increment,
+    nome varchar(100),
+    descricao varchar(500)
+);
+
+-- Cria a tabela de categoria do alimento
+create table tbl_categoria_alimento(
+    id int not null primary key auto_increment,
+    nome varchar(100) not null,
+    descricao varchar(500)
+);
+
+-- Cria a tabela de enquadramento
+create table tbl_enquadramento(
+    id int not null primary key auto_increment,
+    nome varchar(50) not null
+);
+
+-- Cria a tabela de nivel de acesso
+create table tbl_nivel_acesso(
+    id int not null primary key auto_increment,
+    nivel varchar(50) not null
+);
+
+-- Cria a tabela de refeicao relacionada com restricao (Tabela com CRUD)
 create table tbl_refeicao_restricao(
-	id int not null primary key auto_increment,
-	id_refeicao  int not null,
+    id int not null primary key auto_increment,
+    id_refeicao  int not null,
     id_restricao int not null,
     
     # Relacao para a refeicao
-	constraint FK_REFEICAO_REFEICAORESTRICAO
+    constraint FK_REFEICAO_REFEICAORESTRICAO
     foreign key (id_refeicao)
     references tbl_refeicao(id),
     
@@ -74,76 +107,7 @@ create table tbl_refeicao_restricao(
     references tbl_restricao(id)
 );
 
--- Cria a tabela de refeicao relacionada com alimento
-create table tbl_refeicao_alimento(
-	id int not null primary key auto_increment,
-	id_refeicao int not null,
-    id_alimento int not null,
-    quantidade_g decimal(8,2)
-);
-
--- Cria a tabela de adm
-create table tbl_adm(
-	id int not null primary key auto_increment,
-    nome  varchar(100) not null,
-    email varchar(255) not null,
-    senha varchar(255) not null,
-    ultimo_acesso datetime,
-    id_nivel_acesso int not null
-);
-
--- Cria a tabela de recuperacao de senha
-create table tbl_recuperacao_senha(
-	id int not null primary key auto_increment,
-    id_adm int not null,
-    token varchar(255),
-    expira_em datetime,
-    usado tinyint(1)
-);
-
-#########   TABELAS DE DADOS ESTATICOS   #########
-
--- Cria a tabela de tipo_refeicao
-create table tbl_tipo_refeicao(
-	id int not null primary key auto_increment,
-    nome varchar(50)
-);
-
--- Cria a tabela de publico alvo
-create table tbl_publico_alvo(
-	id int not null primary key auto_increment,
-    nome varchar(100)
-);
-
--- Cria a tabela de restricao
-create table tbl_restricao(
-	id int not null primary key auto_increment,
-	nome varchar(100),
-    descricao varchar(500)
-);
-
--- Cria a tabela de categoria do alimento
-create table tbl_categoria_alimento(
-	id int not null primary key auto_increment,
-    nome varchar(100) not null,
-    descricao varchar(500)
-);
-
--- Cria a tabela de enquadramento
-create table tbl_enquadramento(
-	id int not null primary key auto_increment,
-    nome varchar(50) not null
-);
-
--- Cria a tabela de nivel de acesso
-create table tbl_nivel_acesso(
-	id int not null primary key auto_increment,
-    nivel varchar(50) not null
-);
-
-
-#########   INSERCAO DOS DADOS ESTATICOS   #########
-
+#########   INSERÇÃO DOS DADOS ESTATICOS   #########
 -- Insercao de dados da tabela de tipo_refeicao
 insert into tbl_tipo_refeicao(nome)
 values
@@ -177,108 +141,87 @@ values
 -- Insercao de dados na tabela de restricao
 insert into tbl_restricao(nome, descricao)
 values
-(
-    "Intolerância à Lactose",
-    "Dificuldade na digestão da lactose devido à deficiência da enzima lactase."
-),
-(
-    "Doença Celíaca",
-    "Condição autoimune desencadeada pela ingestão de glúten."
-),
-(
-    "Alergia ao Leite",
-    "Reação imunológica às proteínas presentes no leite."
-),
-(
-    "Alergia ao Ovo",
-    "Reação alérgica causada pelo consumo de ovos."
-),
-(
-    "Alergia ao Amendoim",
-    "Reação alérgica provocada pelo consumo de amendoim."
-),
-(
-    "Alergia a Frutos do Mar",
-    "Reação alérgica causada pelo consumo de crustáceos e moluscos."
-),
-(
-    "Hipertensão",
-    "Necessidade de controle da ingestão de sódio."
-),
-(
-    "Diabetes",
-    "Necessidade de controle da ingestão de carboidratos e açúcares."
-);
+("Intolerância à Lactose", "Dificuldade na digestão da lactose devido à deficiência da enzima lactase."),
+("Doença Celíaca", "Condição autoimune desencadeada pela ingestão de glúten."),
+("Alergia ao Leite", "Reação imunológica às proteínas presentes no leite."),
+("Alergia ao Ovo", "Reação alérgica causada pelo consumo de ovos."),
+("Alergia ao Amendoim", "Reação alérgica provocada pelo consumo de amendoim."),
+("Alergia a Frutos do Mar", "Reação alérgica causada pelo consumo de crustáceos e moluscos."),
+("Hipertensão", "Necessidade de controle da ingestão de sódio."),
+("Diabetes", "Necessidade de controle da ingestão de carboidratos e açúcares.");
 
 -- Insercao de dados da tabela de categoria_alimento
 insert into tbl_categoria_alimento(nome, descricao)
 values
-(
-    "Carboidrato Simples",
-    "Carboidratos de rápida absorção que fornecem energia imediata."
-),
-(
-    "Carboidrato Complexo",
-    "Carboidratos de absorção lenta, ricos em fibras e energia sustentada."
-),
-(
-    "Proteína Animal",
-    "Alimentos proteicos de origem animal."
-),
-(
-    "Proteína Vegetal",
-    "Alimentos proteicos de origem vegetal."
-),
-(
-    "Legumes",
-    "Vegetais geralmente consumidos cozidos ou refogados."
-),
-(
-    "Verduras",
-    "Vegetais folhosos ricos em vitaminas e minerais."
-),
-(
-    "Frutas",
-    "Fontes naturais de vitaminas, minerais e fibras."
-),
-(
-    "Oleaginosas",
-    "Castanhas, amêndoas e sementes ricas em gorduras saudáveis."
-),
-(
-    "Laticínios",
-    "Produtos derivados do leite."
-),
-(
-    "Bebidas",
-    "Líquidos destinados ao consumo alimentar."
-),
-(
-    "Temperos e Condimentos",
-    "Ingredientes utilizados para saborizar preparações."
-);
+("Carboidrato Simples", "Carboidratos de rápida absorção que fornecem energia imediata."),
+("Carboidrato Complexo", "Carboidratos de absorção lenta, ricos em fibras e energia sustentada."),
+("Proteína Animal", "Alimentos proteicos de origem animal."),
+("Proteína Vegetal", "Alimentos proteicos de origem vegetal."),
+("Legumes", "Vegetais geralmente consumidos cozidos ou refogados."),
+("Verduras", "Vegetais folhosos ricos em vitaminas e minerais."),
+("Frutas", "Fontes naturais de vitaminas, minerais e fibras."),
+("Oleaginosas", "Castanhas, amêndoas e sementes ricas em gorduras saudáveis."),
+("Laticínios", "Produtos derivados do leite."),
+("Bebidas", "Líquidos destinados ao consumo alimentar."),
+("Temperos e Condimentos", "Ingredientes utilizados para saborizar preparações.");
 
 -- Insercao de dados na tabela de enquadramento
 insert into tbl_enquadramento(nome)
 values
-("Doces"),
-("Processados"),
-("Tuberculos"),
-("Raizes"),
-("Cereais"),
-("Legumes"),
-("Verduras"),
-("Leite"),
-("Derivados"),
-("Aves"),
-("Bovina"),
-("Suina"),
-("Frutos do mar");
+("Doces"), ("Processados"), ("Tuberculos"), ("Raizes"), ("Cereais"), 
+("Legumes"), ("Verduras"), ("Leite"), ("Derivados"), ("Aves"), 
+("Bovina"), ("Suina"), ("Frutos do mar");
 
 -- Insercao de dados na tabela de nivel_acesso
 insert into tbl_nivel_acesso(nivel)
 values
-("Root"),
-("Administrador"),
-("Nutricionista"),
-("Editor");
+("Root"), ("Administrador"), ("Nutricionista"), ("Editor");
+
+#########   ALTER TABLES   #########   
+-- Adiciona a FK na tabela de alimento e cria as relacoes
+alter table tbl_alimento
+    add column id_categoria int not null,
+    add constraint FK_CATEGORIA_ALIMENTO
+        foreign key (id_categoria)
+        references tbl_categoria_alimento(id),
+    add column id_adm int not null,
+    add constraint FK_ADM_ALIMENTO
+        foreign key (id_adm)
+        references tbl_adm(id),
+    add column id_enquadramento int not null,
+    add constraint FK_ENQUADRAMENTO_ALIMENTO
+        foreign key (id_enquadramento)
+        references tbl_enquadramento(id);
+
+-- Adiciona a FK na tabela de refeicao e cria as relacoes
+alter table tbl_refeicao
+    add column id_tipo_refeicao int not null,
+    add constraint FK_TIPO_REFEICAO_REFEICAO
+        foreign key (id_tipo_refeicao)
+        references tbl_tipo_refeicao(id),
+    add column id_publico_alvo int not null,
+    add constraint FK_PUBLICO_ALVO_REFEICAO
+        foreign key (id_publico_alvo)
+        references tbl_publico_alvo(id),
+    add column id_adm int not null,
+    add constraint FK_ADM_REFEICAO
+        foreign key (id_adm)
+        references tbl_adm(id);
+ 
+-- Adiciona a FK na tabela de adm e cria as relações
+alter table tbl_adm
+    add column id_nivel_acesso int not null,
+    add constraint FK_NIVEL_ACESSO_ADM
+        foreign key(id_nivel_acesso)
+        references tbl_nivel_acesso(id);
+        
+-- Adiciona as FK na tabela de refeição_alimento e faz as relações
+alter table tbl_refeicao_alimento
+    add column id_refeicao int not null,
+    add constraint FK_REFEICAO_REFEICAOALIMENTO
+        foreign key(id_refeicao)
+        references tbl_refeicao(id),
+    add column id_alimento int not null,
+    add constraint FK_ALIMENTO_REFEICAOALIMENTO
+        foreign key(id_alimento)
+        references tbl_alimento(id);
